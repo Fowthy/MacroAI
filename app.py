fe64c6ea3458d735a3f1695ea8696a2b0c108a26
Alex Svetoslavov <451875@student.fontys.nl>
2024-01-10 14:10:13 +0100

    Added base streamlit file

---
+++ app.py
@@ -0,0 +1,13 @@
+##############################################################
+#                                                            #
+#                          MacroAI                           #
+#                                                            #
+##############################################################
+
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts.chat import ChatPromptTemplate
+
+
+
+st.title('MacroAI')
