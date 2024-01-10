adb3408be5963d6c92655ffc972ac516b8ea2224
Alex Svetoslavov <451875@student.fontys.nl>
2024-01-10 15:07:38 +0100

    Updated graph chart

--- app.py
+++ app.py
@@ -8,16 +8,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["weight", "protein", "carbs"])
+chart_data = pd.DataFrame(np.random.randn(10, 3), columns=["weight",'desired_weight',"protein", "carbs"])
 
 st.title('MacroAI')
 st.subheader("AI powered nutritionist")
-height = st.text_area('Enter your height:')
-weight = st.text_area('Enter your weight:')
+height = st.sidebar.text_area('Enter your height:')
+weight = st.sidebar.text_area('Enter your weight:')
 desired_weight = st.sidebar.slider('Select desired weight', 50, 200, 64)
 
 
 st.line_chart(
-   chart_data, x="protein", y=["protein", "carbs"], color=["#FFFFFF", "#0000FF"]  # Optional
+   chart_data, x="desired_weight", y=["protein", "carbs"], color=["#FFFFFF", "#0000FF"]  # Optional
 )
 
