3594a225934699771795ad2c9430d68afad049d7
Alex Svetoslavov <451875@student.fontys.nl>
2024-01-10 14:58:00 +0100

    added sample grah

--- app.py
+++ app.py
@@ -5,9 +5,18 @@
 ##############################################################
 
 import streamlit as st
-# from langchain.chat_models import ChatOpenAI
-# from langchain.prompts.chat import ChatPromptTemplate
+import pandas as pd
+import numpy as np
 
-
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["weight", "protein", "carbs"])
 
 st.title('MacroAI')
+st.form_sutitle("AI powered nutritionist")
+height = st.text_area('Enter your height:')
+weight = st.text_area('Enter your weight:')
+desired_weight = st.text_area('Enter your desired weight:')
+
+st.line_chart(
+   chart_data, x="protein", y=["protein", "carbs"], color=["#FF0000", "#0000FF"]  # Optional
+)
+
