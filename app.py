89de8b60c21dfc4c08b36f2b26e219b7e195b18c
Alex Svetoslavov <451875@student.fontys.nl>
2024-01-10 15:00:46 +0100

    fixed error

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import numpy as np
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["weight", "protein", "carbs"])
 
 st.title('MacroAI')
-st.form_sutitle("AI powered nutritionist")
+st.subheader("AI powered nutritionist")
 height = st.text_area('Enter your height:')
 weight = st.text_area('Enter your weight:')
 desired_weight = st.text_area('Enter your desired weight:')
