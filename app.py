c2d33abf4e6b88878929d67ab80c66df9eecb5bd
Alex Svetoslavov <451875@student.fontys.nl>
2024-01-10 14:14:57 +0100

    Commented out libraries

--- app.py
+++ app.py
@@ -5,8 +5,8 @@
 ##############################################################
 
 import streamlit as st
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts.chat import ChatPromptTemplate
+# from langchain.chat_models import ChatOpenAI
+# from langchain.prompts.chat import ChatPromptTemplate
 
 
 
