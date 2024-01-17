87b6981d36d47481ca98b3c70e712df025926e35
Alex Svetoslavov <451875@student.fontys.nl>
2024-01-17 20:14:37 +0100

    Updated tooltip and converted to kg

--- app.py
+++ app.py
@@ -7,28 +7,32 @@ import plotly.express as px
 df = pd.read_csv('recommended_nutrition_full_cleaned.csv').head(50)
 df_food = pd.read_csv('fooddata.csv')
 
+# Convert 'Weight' from pounds to kilograms
+df["Weight"] = df["Weight"] / 2.20462
+
 # User inputs
 st.title('MacroAI')
 st.subheader("AI powered nutritionist")
 height = st.sidebar.number_input('Enter your height:', 120, 220, 181)
-weight = st.sidebar.number_input('Enter your weight:', 40, 120, 64) * 2.20462  # Convert weight to pounds
-desired_weight = st.sidebar.number_input('Enter your desired weight:', 40, 120, 75) * 2.20462  # Convert desired weight to pounds
+weight = st.sidebar.number_input('Enter your weight:', 40, 120, 64)
+desired_weight = st.sidebar.number_input('Enter your desired weight:', 40, 120, 75)
 
 # Create a new DataFrame for the line chart
 chart_data = df[df["Weight"].between(weight, desired_weight)].copy()
 
 # Add a new column to the chart_data DataFrame for the tooltip
-def get_foods_and_protein(x):
+def get_foods_and_protein(protein_goal):
     foods = df_food.sample(3)
     food_names = foods["Shrt_Desc"].tolist()
-    protein_amounts = (x / foods["Protein_(g)"]).tolist()  # Calculate the amount of each food to consume
-    return '<br>'.join([f"{name}: {amount:.2f}g" for name, amount in zip(food_names, protein_amounts)])
+    food_proteins = foods["Protein_(g)"].tolist()  # Protein content per 100g of each food
+    food_amounts = [protein_goal * 100 / protein for protein in food_proteins]  # Amount of each food to consume
+    return '<br>'.join([f"{name}: {amount:.2f}g, {protein:.2f}g protein/100g" for name, amount, protein in zip(food_names, food_amounts, food_proteins)])
 
 chart_data["foods"] = chart_data["Protein"].apply(get_foods_and_protein)
 
 # Create the line chart with tooltips
 fig = px.line(chart_data, x="Weight", y="Protein")
-fig.update_traces(hovertemplate='Weight: %{x}<br>Protein: %{y}<br>Foods:<br>%{customdata}', customdata=chart_data['foods'])
+fig.update_traces(hovertemplate='Weight: %{x}kg<br>Protein: %{y}g<br>Foods:<br>%{customdata}', customdata=chart_data['foods'])
 st.plotly_chart(fig)
 
 # Show the dataframes
