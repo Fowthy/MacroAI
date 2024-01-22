4b264e6809a764ee90fc287afc11faecb2ff6521
Fowthy <xfowth@gmail.com>
2024-01-22 12:56:56 +0100

    Added foods page

---
+++ pages/foods.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import pandas as pd
+
+# Load the data
+df_food = pd.read_csv('../fooddata.csv')
+
+# User inputs
+search_term = st.text_input('Search for a food item:')
+
+# Filter the food items
+filtered_food = df_food[df_food['Shrt_Desc'].str.contains(search_term, case=False, na=False)]
+
+# Display the food items in a grid
+cols = st.beta_columns(3)
+selected_foods = []
+for i, food in enumerate(filtered_food['Shrt_Desc']):
+    if cols[i % 3].checkbox(food):
+        selected_foods.append(food)
+
+# Display the selected food items
+st.write('You selected:')
+for food in selected_foods:
+    st.write(food)
