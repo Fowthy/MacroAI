cd9846a593f7a2069c5037ca9fd72cf64f3ac864
Fowthy <xfowth@gmail.com>
2024-01-22 13:05:48 +0100

    test

--- pages/foods.py
+++ pages/foods.py
@@ -3,6 +3,12 @@ import pandas as pd
 
 # Load the data
 df_food = pd.read_csv('./fooddata.csv').head(100)
+selected_foods = []
+
+# Display the selected food items
+st.write('You selected:')
+for food in selected_foods:
+    st.write(food)
 
 # User inputs
 search_term = st.text_input('Search for a food item:')
@@ -12,12 +18,6 @@ filtered_food = df_food[df_food['Shrt_Desc'].str.contains(search_term, case=Fals
 
 # Display the food items in a grid
 cols = st.columns(3)
-selected_foods = []
 for i, food in enumerate(filtered_food['Shrt_Desc']):
     if cols[i % 3].checkbox(food):
         selected_foods.append(food)
-
-# Display the selected food items
-st.write('You selected:')
-for food in selected_foods:
-    st.write(food)
